9e8bf95b265c62be4040ea76124b490295f13ae7
1350358138 <1350358138@qq.com>
2022-08-05 15:29:26 +0800

    Add project files.

---
+++ FIT5032_MyViewModel/Models/SampleFormModels.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+namespace FIT5032_MyViewModels.Models
+{
+    public class SampleFormViewModels
+    {
+    }
+    public class FormOneViewModel
+    {
+        [Required]
+        [Display(Name = "First Name")]
+        public string FirstName { get; set; }
+        [Required]
+        public string LastName { get; set; }
+    }
+}
